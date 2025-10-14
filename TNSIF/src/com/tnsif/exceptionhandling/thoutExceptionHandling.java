6821704bd786bb49812d442f73a72df53eee9060
sudha murugan <sudha murugan@DESKTOP-4EVSH9H>
2025-10-14 22:05:00 +0530

    project7

---
+++ TNSIF/src/com/tnsif/exceptionhandling/thoutExceptionHandling.java
@@ -0,0 +1,9 @@
+package com.tnsif.exceptionhandling;
+
+public class thoutExceptionHandling {
+	public static void main(String[] args) {
+		System.out.println("the program continues....");
+		int data=100/0;
+	}
+	
+}
