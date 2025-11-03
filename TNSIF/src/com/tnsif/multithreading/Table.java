5d020ffc51253faf77207074e53214a98be2fc80
sudha murugan <sudha murugan@DESKTOP-4EVSH9H>
2025-11-03 18:15:27 +0530

    project11

--- TNSIF/src/com/tnsif/multithreading/Table.java
+++ TNSIF/src/com/tnsif/multithreading/Table.java
@@ -1,5 +1,42 @@
 package com.tnsif.multithreading;
 
 public class Table {
-
+	synchronized  void printTable(int n) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(n + " x " + i + " = " + (n * i));
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+        }
+    }
 }
+
+// Thread class 1
+class MyThread1 extends Thread {
+    Table t;
+
+    MyThread1(Table t) {
+        this.t = t;
+    }
+
+    public void run() {
+        t.printTable(5);
+    }
+}
+
+// Thread class 2
+class MyThread2 extends Thread {
+    Table t;
+
+    MyThread2(Table t) {
+        this.t = t;
+    }
+
+    public void run() {
+        t.printTable(100);
+    }
+}
+
+
--- TNSIF/src/com/tnsif/multithreading/TestSynchronization.java
+++ TNSIF/src/com/tnsif/multithreading/TestSynchronization.java
@@ -1,5 +1,14 @@
 package com.tnsif.multithreading;
 
 public class TestSynchronization {
+	public static void main(String[] args) {
+        Table obj = new Table(); // Shared object
 
+        MyThread1 t1 = new MyThread1(obj);
+        MyThread2 t2 = new MyThread2(obj);
+
+        t1.start();
+        t2.start();
+    }
 }
+
